68ae763db7216b1cda24cfd2bd2978ddc2aef83e
hdt <ngdathd@gmail.com>
2019-03-12 01:46:22 +0700

    first

---
+++ app/src/main/java/vn/misa/nadat/misademo5food/DetailRestaurantActivity.java
@@ -0,0 +1,98 @@
+package vn.misa.nadat.misademo5food;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DetailRestaurantActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_restaurant);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_left_arrow);
+        }
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle(getResources().getString(R.string.food_name));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+
+        findViewById(R.id.ln_take_photo).setOnClickListener(this);
+        findViewById(R.id.ln_check_in).setOnClickListener(this);
+        findViewById(R.id.ln_save).setOnClickListener(this);
+        findViewById(R.id.ln_telephone).setOnClickListener(this);
+        findViewById(R.id.ln_local).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings: {
+                break;
+            }
+            case R.id.action_share: {
+                break;
+            }
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ln_take_photo: {
+                break;
+            }
+            case R.id.ln_check_in: {
+                break;
+            }
+            case R.id.ln_save: {
+                break;
+            }
+            case R.id.ln_telephone: {
+                break;
+            }
+            case R.id.ln_local: {
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
